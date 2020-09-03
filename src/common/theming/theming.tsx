199e0dba6f4852041a00c3810ea19b34ee8966d4
Mayke <mayke_sargento@hotmail.com>
2020-09-03 11:43:35 -0300

    configuring some theme props

--- src/common/theming/theming.tsx
+++ src/common/theming/theming.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { makeStyles, createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
@@ -8,14 +9,65 @@ export const theme = createMuiTheme({
         secondary: {
             main: '#EDED85',
         },
+        // @ts-ignore
+        gray: {
+            '94': 'F0F0F0',
+            bg: '#FAFAFA',
+            darker: '#D8D8D8',
+            darkest: '#ededed',
+        },
+        text: {
+            primary: '#20352e',
+            secondary: '#757575',
+        },
     },
     typography: {
         fontFamily: 'Poppins',
+        h1: {
+            fontSize: 40,
+            letterSpacing: 0,
+            lineHeight: 1.6,
+            fontWeight: 900,
+        },
+        h2: {
+            fontSize: 35,
+            letterSpacing: 0,
+            lineHeight: 1.2,
+            fontWeight: 900,
+        },
+        h3: {
+            fontSize: 25,
+            letterSpacing: 0,
+            lineHeight: 1.2,
+            fontWeight: 900,
+        },
+        overline: {
+            fontSize: 20,
+            letterSpacing: 0,
+            lineHeight: 1.2,
+            fontWeight: 900,
+        },
         button: {
             textTransform: 'none',
         },
     },
+    overrides: {
+        MuiButton: {
+            root: {
+                fontWeight: 900,
+                fontSize: 16,
+                borderRadius: 15,
+                padding: '8px 16px',
+            },
+            sizeLarge: {
+                fontSize: 18,
+                borderRadius: 25,
+                padding: '18px 25px',
+            },
+        },
+    },
 });
+
 export const useStyles = makeStyles({
     root: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -30,5 +82,7 @@ export const useStyles = makeStyles({
         borderRadius: 15,
         paddingLeft: '25px',
         paddingRight: '25px',
+        fontWeight: 900,
+        fontSize: 14,
     },
 });
