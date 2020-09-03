8c4bc66185793be5152edf9e176e2beba2e50810
Mayke <mayke_sargento@hotmail.com>
2020-09-03 11:44:48 -0300

    adding auth page

--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import { Grid, Box } from 'react-feather';
-import './auth-page.scss';
+import { RouteComponentProps } from '@reach/router';
+import { Box, makeStyles, Typography, Button, Grid } from '@material-ui/core';
+import logo from '../../logo.svg';
 
-export function AuthPage() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function AuthPage(props: RouteComponentProps) {
+    const styles = useStyles();
+
     return (
-        <Container maxWidth="xl" className="">
-            <Grid style={{ minHeight: '100vh', width: '100%' }}>
-                <Box className="view-cotaniner" />
-            </Grid>
-        </Container>
+        <Box width={1} height="100vh" display="flex" alignItems="center" justifyContent="center" bgcolor="primary.main">
+            <Box width="480px" bgcolor="gray.bg" borderRadius={40} p={5}>
+                <Box display="flex" flexDirection="column" alignItems="center">
+                    <Box py={3}>
+                        <img src={logo} alt="Ollie" className={styles.logo} />
+                    </Box>
+                    <Box textAlign="center" mb={6}>
+                        <Typography variant="h1">welcome to ollie</Typography>
+                        <Typography variant="h3">more appointments</Typography>
+                    </Box>
+                    <Grid container spacing={3}>
+                        <Grid item xs>
+                            <Button variant="text" color="primary" size="large" fullWidth>
+                                log in
+                            </Button>
+                        </Grid>
+                        <Grid item xs>
+                            <Button variant="contained" color="primary" size="large" fullWidth>
+                                sign up
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Box>
     );
 }
+
+const useStyles = makeStyles({
+    logo: {
+        width: '180px',
+        height: 'auto',
+    },
+});
