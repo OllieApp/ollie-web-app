22739d999b37c5c883f877924615b3d9c5b0b3a0
Mayke <mayke_sargento@hotmail.com>
2020-09-09 10:03:10 -0300

    adding signup steps validation and refactoring to useField

--- src/pages/signup-page/components/name-step.tsx
+++ src/pages/signup-page/components/name-step.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Box, Grid, TextField } from '@material-ui/core';
+import { useField } from 'formik';
 import { StepViewProps } from '../types';
 import { StepHeader } from './step-header';
 
-export function NameStep({ values, errors, step, onChange, onBlur }: StepViewProps) {
+export function NameStep({ step }: StepViewProps) {
+    const [firstNameField, firstNameMeta] = useField({ name: 'firstName', title: 'first name' });
+    const [lastNameField, lastNameMeta] = useField({ name: 'lastName', title: 'last name' });
+
     return (
         <Box pt={4} pb={6}>
             <StepHeader step={step + 1} title="What is your name?" />
@@ -11,25 +15,22 @@ export function NameStep({ values, errors, step, onChange, onBlur }: StepViewPro
                 <Grid container spacing={3}>
                     <Grid item xs>
                         <TextField
-                            name="firstName"
+                            {...firstNameField}
                             variant="filled"
                             placeholder="Name"
-                            value={values.firstName}
-                            error={!!errors.firstName}
-                            onChange={onChange}
-                            onBlur={onBlur}
+                            error={!!(firstNameMeta.touched && firstNameMeta.error)}
+                            helperText={firstNameMeta.touched ? firstNameMeta.error : ''}
+                            autoFocus
                             fullWidth
                         />
                     </Grid>
                     <Grid item xs>
                         <TextField
-                            name="lastName"
+                            {...lastNameField}
                             variant="filled"
                             placeholder="Surname"
-                            value={values.lastName}
-                            error={!!errors.lastName}
-                            onChange={onChange}
-                            onBlur={onBlur}
+                            error={!!(lastNameMeta.touched && lastNameMeta.error)}
+                            helperText={lastNameMeta.touched ? lastNameMeta.error : ''}
                             fullWidth
                         />
                     </Grid>
--- src/pages/signup-page/types.ts
+++ src/pages/signup-page/types.ts
@@ -1,18 +1,5 @@
-export interface FormState {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    category: string;
-    gender: string;
-}
-
 export interface StepViewProps {
-    values: Partial<FormState>;
-    errors: Partial<FormState>;
     step: number;
-    onChange: (event: React.ChangeEvent<unknown>) => void;
-    onBlur: (event: React.FocusEvent<unknown>) => void;
 }
 
 export interface StepConfig {
