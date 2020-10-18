9bee3005459b4a2159e983f84aa8fb628de64691
Mayke <mayke_sargento@hotmail.com>
2020-10-18 03:03:03 -0300

    integrating userStore with ollie api

--- src/common/stores/user-store.ts
+++ src/common/stores/user-store.ts
@@ -1,60 +1,60 @@
 import { observable, action, computed } from 'mobx';
 import firebase from 'firebase';
-import { auth, firestore } from '../firebase/firebase-wrapper';
+import { auth } from '../firebase/firebase-wrapper';
 import { RootStore } from './root-store';
-import { User, AuthUser } from '../../types';
+import { User, AuthUser, Practitioner } from '../../types';
 import { OllieAPI } from '../api';
 
 export default class UserStore {
     rootStore: RootStore;
 
-    @observable user: firebase.User | null;
+    @observable firebaseUser: firebase.User | null;
 
-    @observable userInfo: User | null;
+    @observable user: User | null;
+
+    @observable practitionerIds: string[] | null;
+
+    @observable practitionerInfo: Practitioner | null;
+
+    @observable isLoadingUserInfo = false;
 
     @computed get isAuthenticated() {
         return this.user !== null;
     }
 
     @computed get isActive() {
-        return this.userInfo?.active || false;
+        return this.practitionerInfo?.isActive || false;
     }
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
         this.user = null;
-        this.userInfo = null;
+        this.firebaseUser = null;
+        this.practitionerIds = null;
+        this.practitionerInfo = null;
         firebase.auth().onAuthStateChanged((user) => {
-            this.user = user;
+            this.firebaseUser = user;
         });
     }
 
     @action async loginWithEmail({ email, password }: { email: string; password: string }): Promise<void> {
         await auth().signInWithEmailAndPassword(email, password);
+        await this.fetchUserInfo();
     }
 
     @action async signUpWithEmail({ email, password, firstName, lastName, gender, category }: AuthUser): Promise<void> {
         const { user } = await auth().createUserWithEmailAndPassword(email, password);
         if (!user) throw new Error('Error creating user with email.');
 
-        const payload = {
+        await OllieAPI.post('/practitioners', {
             firstName,
             lastName,
             email,
-            gender,
             category,
-        };
+            gender,
+        });
 
-        await OllieAPI.post('/practitioners', payload);
-
-        // await firestore().collection('users').doc(user.uid).set({
-        //     firstName,
-        //     lastName,
-        //     email,
-        //     gender,
-        //     category,
-        //     active: false,
-        // });
+        await this.fetchUserInfo();
     }
 
     @action async logout(): Promise<void> {
@@ -62,8 +62,40 @@ export default class UserStore {
     }
 
     @action async fetchUserInfo(): Promise<void> {
-        if (!this.user) return;
-        const res = await firestore().collection('users').doc(this.user.uid).get();
-        this.userInfo = res.data() as User;
+        if (!this.firebaseUser) return;
+
+        this.isLoadingUserInfo = true;
+
+        const { data: user } = await OllieAPI.get<User>(`/users`);
+        const {
+            data: { ids },
+        } = await OllieAPI.get<{ ids: string[] }>(`/practitioners`);
+        const { data: practitioner } = await OllieAPI.get(`/practitioners/${ids[0]}`);
+
+        this.user = user as User;
+        this.practitionerIds = ids;
+        this.practitionerInfo = practitioner as Practitioner;
+        this.isLoadingUserInfo = false;
+    }
+
+    @action async updatePractitionerProfile(data: Practitioner): Promise<void> {
+        if (!this.practitionerIds) return;
+
+        await OllieAPI.patch<Practitioner>(`/practitioners/${this.practitionerIds[0]}`, {
+            data,
+        });
+    }
+
+    @action async uploadAvatar(file: File): Promise<void> {
+        if (!this.practitionerIds) return;
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        await OllieAPI.post<Practitioner>(`/practitioners/avatar/${this.practitionerIds[0]}`, formData, {
+            headers: { 'Content-Type': 'multipart/form-data' },
+        });
+
+        await this.fetchUserInfo();
     }
 }
