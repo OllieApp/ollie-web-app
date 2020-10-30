cf8f0ab6d855523744e5a0b9662f885aeb096f73
Mayke <mayke_sargento@hotmail.com>
2020-10-30 08:51:54 -0300

    configuring ws for apollo

--- src/common/apollo/index.ts
+++ src/common/apollo/index.ts
@@ -1,9 +1,18 @@
 import { useCallback } from 'react';
-import { ApolloClient, InMemoryCache, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, NormalizedCacheObject, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { OperationDefinitionNode } from 'graphql';
 
 export const useApolloClient = (authToken?: string): ApolloClient<NormalizedCacheObject> => {
     const createApolloClient = useCallback((): ApolloClient<NormalizedCacheObject> => {
-        const link = new HttpLink({
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+            'X-Hasura-Role': 'practitioner',
+        };
+
+        const httpLink = new HttpLink({
             uri: process.env.REACT_APP_OLLIE_GRAPHQL_URL,
             fetch: (uri, options) => {
                 const opt = options || {};
@@ -27,12 +36,26 @@ export const useApolloClient = (authToken?: string): ApolloClient<NormalizedCach
             },
         });
 
+        const client = new SubscriptionClient(process.env.REACT_APP_OLLIE_GRAPHQL_WS_URL as string, {
+            reconnect: true,
+            connectionParams: {
+                headers,
+            },
+        });
+
+        const wsLink = new WebSocketLink(client);
+
+        const link = split(
+            ({ query }) => {
+                const { kind, operation } = getMainDefinition(query) as OperationDefinitionNode;
+                return kind === 'OperationDefinition' && operation === 'subscription';
+            },
+            wsLink,
+            httpLink,
+        );
+
         return new ApolloClient({
             link,
-            headers: {
-                Authorization: `Bearer ${authToken}`,
-                'X-Hasura-Role': 'practitioner',
-            },
             cache: new InMemoryCache(),
         });
     }, [authToken]);
