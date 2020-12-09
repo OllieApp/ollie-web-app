fc87b5ed5719a939e7a7f36d9d3393609e386141
Mayke <mayke_sargento@hotmail.com>
2020-12-09 06:42:39 -0300

    refactorign userStore

--- src/common/stores/user-store.ts
+++ src/common/stores/user-store.ts
@@ -10,7 +10,7 @@ export default class UserStore {
 
   @observable authStatus: 'in' | 'out' | 'loading' = 'out';
 
-  @observable authMethod?: 'email' | 'google';
+  @observable authMethod?: 'email' | 'google' = 'email';
 
   @observable firebaseUser?: firebase.User;
 
@@ -22,70 +22,117 @@ export default class UserStore {
 
   @observable practitionerInfo?: Practitioner;
 
-  @observable isLoadingFirebaseUser = true;
+  @observable isBootstraped = false;
 
   @observable isSignInWithEmailAndPasswordLoading = false;
 
+  @observable isSignInWithEmailLoading = false;
+
   @observable isSignInWithGoogleLoading = false;
 
-  @observable isLoadingPractitionerInfo = false;
+  @observable isSignUpWithEmailLoading = false;
+
+  @observable isFetchingUserInfo = false;
 
   @observable userExists = false;
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
 
-    auth().onAuthStateChanged((user) => this.updateFirebaseUser(user));
+    const { currentUser } = auth();
+    if (currentUser) this.fetchUserInfo(currentUser);
+
+    auth().onAuthStateChanged((user) => {
+      if (!this.isBootstraped) this.bootstrap(user);
+    });
+  }
+
+  async bootstrap(user?: firebase.User | null) {
+    if (user && this.authStatus === 'out') {
+      await this.checkUserExistence();
+      if (this.userExists) await this.fetchUserInfo(user);
+    }
+
+    this.isBootstraped = true;
   }
 
   @computed get isAuthenticated(): boolean {
-    return this.authStatus === 'in' && Boolean(this.authToken && this.user && this.practitionerInfo);
+    return this.authStatus === 'in';
   }
 
   @computed get isLoadingAuth(): boolean {
-    return this.isLoadingFirebaseUser || this.isLoadingPractitionerInfo;
+    return this.authStatus === 'loading' || this.isFetchingUserInfo;
   }
 
   @computed get isActive(): boolean {
     return this.practitionerInfo?.isActive || false;
   }
 
-  @computed get hasPractitionersCreated(): boolean {
-    return Boolean(this.firebaseUser && this.practitionerIds?.length);
+  @computed get isCommingFromGAuth(): boolean {
+    return Boolean(this.firebaseUser && !this.userExists);
   }
 
   @action async signInWithEmailAndPassword({ email, password }: { email: string; password: string }): Promise<void> {
     try {
+      this.authMethod = 'email';
       this.isSignInWithEmailAndPasswordLoading = true;
-      this.isLoadingFirebaseUser = true;
-      await auth().signInWithEmailAndPassword(email, password);
-      this.isLoadingFirebaseUser = false;
-      await this.fetchUserInfo();
-      this.isSignInWithEmailAndPasswordLoading = false;
+
+      const { user } = await auth().signInWithEmailAndPassword(email, password);
+      if (!user) throw new Error('Error siging with email and password.');
+
+      this.fetchUserInfo(user);
     } catch (ex) {
       this.isSignInWithEmailAndPasswordLoading = false;
       throw ex;
     }
   }
 
-  @action async signUpWithEmail({ email, firstName, lastName, gender, category }: Partial<AuthUser>): Promise<void> {
+  @action async signInWithGoogle() {
     try {
-      this.isLoadingPractitionerInfo = true;
+      this.authMethod = 'google';
+      this.isSignInWithGoogleLoading = true;
+
+      const provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+
+      const { user } = await auth().signInWithPopup(provider);
+      if (!user) throw new Error('Error creating user with Google.');
+
+      this.firebaseUser = user;
+
+      await this.checkUserExistence();
+      if (this.userExists) await this.fetchUserInfo(user);
+
+      return {
+        userExists: this.userExists,
+      };
+    } catch (ex) {
+      this.authMethod = 'email';
+      this.isSignInWithGoogleLoading = false;
+      throw ex;
+    }
+  }
+
+  @action async signUpWithEmail(
+    { email, firstName, lastName, gender, category }: Partial<AuthUser>,
+    user = this.firebaseUser,
+  ): Promise<void> {
+    try {
+      this.authStatus = 'loading';
+      this.isSignUpWithEmailLoading = true;
 
       await OllieAPI.post('/practitioners', {
         firstName,
         lastName,
-        email: email || this.firebaseUser?.email,
+        email: email || user?.email,
         category,
         gender,
       });
 
-      if (this.firebaseUser) {
-        await this.updateFirebaseUser(this.firebaseUser);
-        await this.fetchUserInfo();
-      }
+      await this.fetchUserInfo(user);
     } catch (ex) {
-      this.isLoadingPractitionerInfo = false;
+      this.authStatus = 'out';
+      this.isSignUpWithEmailLoading = false;
       throw ex;
     }
   }
@@ -99,72 +146,47 @@ export default class UserStore {
     category,
   }: AuthUser): Promise<void> {
     try {
-      this.authMethod = 'google';
-      this.isLoadingFirebaseUser = true;
+      this.authStatus = 'loading';
+      this.isSignUpWithEmailLoading = true;
 
-      if (!email || !password) {
-        throw new Error('Missing required email or password');
-      }
+      if (!email || !password) throw new Error('Missing required email or password');
 
       const { user } = await auth().createUserWithEmailAndPassword(email, password);
       if (!user) throw new Error('Error creating user with email.');
 
-      this.firebaseUser = user;
-      this.isLoadingFirebaseUser = false;
-
-      this.signUpWithEmail({
-        firstName,
-        lastName,
-        gender,
-        category,
-      });
+      await this.signUpWithEmail(
+        {
+          email: user.email || undefined,
+          firstName,
+          lastName,
+          gender,
+          category,
+        },
+        user,
+      );
     } catch (ex) {
-      this.isLoadingFirebaseUser = false;
+      this.authStatus = 'out';
+      this.isSignUpWithEmailLoading = false;
       throw ex;
     }
   }
 
-  @action async signInWithGoogle() {
-    try {
-      this.authMethod = 'google';
-      this.isSignInWithGoogleLoading = true;
-      this.isLoadingFirebaseUser = true;
-
-      const provider = new firebase.auth.GoogleAuthProvider();
-      provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-
-      const { user } = await auth().signInWithPopup(provider);
-      if (!user) throw new Error('Error creating user with Google.');
-
-      this.firebaseUser = user;
-      this.isLoadingFirebaseUser = false;
-
-      await this.checkUserExistence();
-
-      if (this.userExists) {
-        await this.fetchUserInfo();
-      }
-
-      this.isSignInWithGoogleLoading = false;
-    } catch (ex) {
-      this.isSignInWithGoogleLoading = false;
-      this.isLoadingFirebaseUser = false;
-      throw ex;
-    }
-  }
-
-  @action async logout(): Promise<void> {
+  @action async signOut(): Promise<void> {
     await auth().signOut();
-    this.user = null;
-    this.practitionerInfo = undefined;
+
+    this.user = undefined;
+    this.authToken = undefined;
     this.firebaseUser = undefined;
+    this.practitionerIds = undefined;
+    this.practitionerInfo = undefined;
+    this.authStatus = 'out';
   }
 
   @action async updatePractitionerProfile(data: Partial<Practitioner> & Partial<User>): Promise<void> {
     if (!this.practitionerIds) return;
 
     await OllieAPI.patch<Practitioner>(`/practitioners/${this.practitionerIds[0]}`, data);
-    await this.fetchUserInfo();
+    await this.fetchPractitionerInfo();
   }
 
   @action async uploadAvatar(file: File): Promise<void> {
@@ -177,7 +199,7 @@ export default class UserStore {
       headers: { 'Content-Type': 'multipart/form-data' },
     });
 
-    await this.fetchUserInfo();
+    await this.fetchPractitionerInfo();
   }
 
   @action async cancelAppointment(id: string): Promise<void> {
@@ -186,56 +208,25 @@ export default class UserStore {
     await OllieAPI.post(`/appointments/${id}/cancel`);
   }
 
-  @action async validateAuth(): Promise<void> {
-    if (this.authStatus === 'in' && !this.practitionerInfo && !this.isLoadingPractitionerInfo) {
-      await this.fetchUserInfo();
-    }
-  }
+  async watchToken() {
+    const user = this.firebaseUser;
+    if (!user) return;
 
-  async updateFirebaseUser(user: firebase.User | null) {
-    if (user) {
-      const token = await user.getIdToken(true);
-      const idTokenResult = await user.getIdTokenResult();
-      const hasuraClaim = idTokenResult.claims['https://hasura.io/jwt/claims'];
-
-      if (hasuraClaim) {
-        this.firebaseUser = user;
-        this.authStatus = 'in';
-        this.authToken = token;
-      } else {
-        // Check if refresh is required.
-        const metadataRef = database().ref(`metadata/${user.uid}/refreshTime`);
-
-        metadataRef.on('value', async (data) => {
-          if (!data.exists) return;
-          // Force refresh to pick up the latest custom claims changes.
-          const newToken = await user.getIdToken(true);
-
-          this.firebaseUser = user;
-          this.authStatus = 'in';
-          this.authToken = newToken;
-          this.isLoadingFirebaseUser = false;
-        });
-      }
-    } else {
-      this.firebaseUser = undefined;
-      this.authStatus = 'out';
-      this.authToken = undefined;
-    }
-
-    this.isLoadingFirebaseUser = false;
+    const metadataRef = database().ref(`metadata/${user.uid}/refreshTime`);
+    metadataRef.on('value', async (data) => {
+      if (!data.exists) return;
+      // Force refresh to pick up the latest custom claims changes.
+      await user.getIdToken(true);
+      this.fetchUserInfo(user);
+    });
   }
 
   async checkUserExistence(): Promise<void> {
-    if (!this.firebaseUser) {
-      this.userExists = false;
-      return;
-    }
-
     try {
       await OllieAPI.get<User>(`/users`);
       this.userExists = true;
     } catch (ex) {
+      console.log(ex.response?.status);
       if (ex.response?.status === 404) {
         this.userExists = false;
       } else {
@@ -244,28 +235,34 @@ export default class UserStore {
     }
   }
 
-  async fetchPractitionerInfo(): Promise<void> {
-    if (!this.firebaseUser) return;
-
-    this.isLoadingPractitionerInfo = true;
-
+  async fetchPractitionerInfo() {
     const {
       data: { ids },
     } = await OllieAPI.get<{ ids: string[] }>(`/practitioners`);
-
     const { data: practitioner } = await OllieAPI.get(`/practitioners/${ids[0]}`);
+
     this.practitionerIds = ids;
     this.practitionerInfo = practitioner as Practitioner;
-    this.isLoadingPractitionerInfo = false;
   }
 
-  async fetchUserInfo(): Promise<void> {
-    if (!this.firebaseUser) return;
-
+  async fetchUserInfo(firebaseUser = this.firebaseUser): Promise<void> {
+    this.isFetchingUserInfo = true;
     const { data: user } = await OllieAPI.get<User>(`/users`);
 
-    this.user = user as User;
-
     await this.fetchPractitionerInfo();
+
+    this.user = user as User;
+    this.firebaseUser = firebaseUser;
+    this.authToken = await firebaseUser?.getIdToken();
+    this.userExists = true;
+    this.isSignInWithGoogleLoading = false;
+    this.isSignInWithEmailLoading = false;
+    this.isSignInWithEmailAndPasswordLoading = false;
+    this.isSignUpWithEmailLoading = false;
+    this.isFetchingUserInfo = false;
+    this.isBootstraped = true;
+    this.authStatus = 'in';
+
+    this.watchToken();
   }
 }
