b2bae4b8bba394a5cb7cafcac14c9e72ed75f2bd
aac <andrey_europe_95hd@yahoo.com>
2021-06-14 23:07:09 +0200

    Changes in client creation

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -1,23 +1,25 @@
 import Axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
 export class OllieAPI {
-  static token?: string;
-
-  static async getClient(): Promise<AxiosInstance> {
-    if (!this.token) {
-      this.token = await firebase.auth().currentUser?.getIdToken();
-    }
-
-    return Axios.create({
+  static getClient(): AxiosInstance {
+    const client = Axios.create({
       baseURL: process.env.REACT_APP_OLLIE_API_URL,
-      headers: { Authorization: `bearer ${this.token}` },
     });
+    client.interceptors.request.use(async (request) => {
+      request.headers = { Authorization: `Bearer ${await firebase.auth().currentUser?.getIdToken()}` };
+      return request;
+    });
+
+    return client;
   }
 
   static async get<T = unknown>(path: string, options?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    const client = await this.getClient();
-    return client.get<T>(path, options);
+    const client = this.getClient();
+    return client.get<T>(path, {
+      ...options,
+      headers: await firebase.auth().currentUser?.getIdToken(),
+    });
   }
 
   static async post<T = unknown>(
@@ -25,7 +27,7 @@ export class OllieAPI {
     data?: Record<string, unknown> | FormData,
     options?: AxiosRequestConfig,
   ): Promise<AxiosResponse<T>> {
-    const client = await this.getClient();
+    const client = this.getClient();
     return client.post<T>(path, data, options);
   }
 
@@ -34,21 +36,21 @@ export class OllieAPI {
     data: Record<string, unknown> | FormData,
     options?: AxiosRequestConfig,
   ): Promise<AxiosResponse<T>> {
-    const client = await this.getClient();
+    const client = this.getClient();
     return client.put<T>(path, data, options);
   }
 
   static async patch<T = unknown>(
     path: string,
-    data: Record<string, unknown> | FormData,
+    data: Record<string, unknown> | FormData | any,
     options?: AxiosRequestConfig,
   ): Promise<AxiosResponse<T>> {
-    const client = await this.getClient();
+    const client = this.getClient();
     return client.patch<T>(path, data, options);
   }
 
   static async delete<T = unknown>(path: string, options?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    const client = await this.getClient();
+    const client = this.getClient();
     return client.delete<T>(path, options);
   }
 }
